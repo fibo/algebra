ca85c292142243cb7517a91d67408557f095d096
fibo <casati_gianluca@yahoo.it>
2012-09-11 22:35:49 +0200

    added matrix

--- test/matrix.js
+++ test/matrix.js
@@ -1,13 +1,39 @@
 
 var assert = require('assert');
+var Matrix = require('../index.js').Matrix;
 
 describe('Matrix', function () {
+  describe('identity', function () {
+    it('is the default identity matrix', function () {
 
-  describe('constructor', function () {
-
-    it('accepts one integer as dimension parameter', function () {
+      var id = new Matrix();
+      assert.equal(1, id.data[0][0]);
+      assert.equal(1, id.ij(0,0));
+      var i = 1;
+      assert.equal(0, id.ij(0,i));
+      var data = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+      ];
+      assert.deepEqual(id.data,data);
 
     });
   });
+  describe('constructor', function () {
+    it('accepts one integer as dimension parameter', function () {
 
+      var dim = 3;
+      var m2 = new Matrix(dim);
+      assert.equal(dim, m2.dim);
+
+      it('which defaults to 4', function () {
+
+        var m1 = new Matrix();
+        assert.equal(4, m1.dim);
+
+      });
+    });
+  });
 });

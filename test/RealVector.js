569f51ce643a5b3234cbfd336b2e6aa554dbcd46
fibo <casati_gianluca@yahoo.it>
2012-09-17 00:04:14 +0200

    this is the right way

--- test/RealVector.js
+++ test/RealVector.js
@@ -4,10 +4,17 @@ var RealVectorSpace = require('../index.js').Real.VectorSpace;
 
 var R2 = new RealVectorSpace(2);
 
-var v1 = R2.Vector(1,1);
-var zero = R2.getZero();
+var a = 1;
+var b = 2;
+var v1 = R2.Vector(a,b);
 
 describe('RealVector', function () {
-
+  it('has an accessor to get a single element', function() {
+    assert.equal(v1.x(0),a);
+    assert.equal(v1.x(1),b);
+  });
+  it('has the same dim as its vector space', function() {
+    assert.equal(v1.getDim(),R2.getDim());
+  });
 });
 

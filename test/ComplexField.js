d203045819e4ddbf5aac50405ce12417c4bb2896
fibo <casati_gianluca@yahoo.it>
2012-10-07 15:38:38 +0200

    added complex units

--- test/ComplexField.js
+++ test/ComplexField.js
@@ -19,19 +19,59 @@ describe('ComplexField', function () {
     });
   });
 
-  it('has a zero and a one element', function() {
-    var one = new Complex(1);
-    one.mul(one);
-    assert.equal(one.re(), 1);
-    assert.equal(one.im(), 0);
-
-    var zero = new Complex(0);
-    zero.mul(one);
-    assert.equal(zero.re(), 0);
-    assert.equal(zero.im(), 0);
+  describe('getZero()', function () {
+    it('returns the 0 element', function() {
+      var zero = C.getZero();
+      assert.equal(zero.re(), 0);
+      assert.equal(zero.im(), 0);
+    });
   });
 
-  it('defines + * - / operations', function() {
+  describe('getOne()', function () {
+    it('returns the 1 element', function() {
+      var one = C.getOne();
+      assert.equal(one.re(), 1);
+      assert.equal(one.im(), 0);
+    });
+
+    it('is the multiplication neutral element', function() {
+      var one = C.getOne();
+      one.mul(one);
+      assert.equal(one.re(), 1);
+      assert.equal(one.im(), 0);
+    });
+  });
+
+  describe('getMinusOne()', function () {
+    it('returns the -1 element', function() {
+      var minusOne = C.getMinusOne();
+      minusOne.mul(minusOne);
+      assert.equal(minusOne.re(), 1);
+      assert.equal(minusOne.im(), 0);
+    });
+  });
+
+  describe('getI()', function () {
+    it('returns the i element', function() {
+      var i = C.getI();
+      assert.equal(i.re(), 0);
+      assert.equal(i.im(), 1);
+    });
+
+    it('is the -1 square root', function() {
+      var i = C.getI();
+      // TODO non funziona var minusOne = C.mul(i, i);
+      var minusOne = i.mul(i);
+      assert.ok(minusOne.eq(C.getMinusOne()));
+    });
+  });
+
+  describe('getMinusI()', function () {
+    it('returns the -i element', function() {
+      var minusI = C.getMinusI();
+      assert.equal(minusI.re(), 0);
+      assert.equal(minusI.im(), -1);
+    });
   });
 });
 

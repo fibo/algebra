581157bd730baae3793ddc9686d82decd5b307f3
fibo <casati_gianluca@yahoo.it>
2012-11-02 15:30:53 +0100

    added MatrixRing

---
+++ examples/RealMatrix.js
@@ -0,0 +1,7 @@
+
+var algebra = require('algebra');
+
+var MnR = algebra.Real.MatrixRing;
+
+var M2R = new MnR(2);
+
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
-exports.Field              = require('./lib/Field.js');
-exports.Matrix             = require('./lib/Matrix.js');
-exports.SquareMatrix       = require('./lib/SquareMatrix.js');
-exports.GeneralLinearGroup = require('./lib/GeneralLinearGroup.js');
-exports.Group              = require('./lib/Group.js');
-exports.Vector             = require('./lib/Vector.js');
-exports.VectorSpace        = require('./lib/VectorSpace.js');
+exports.Field        = require('./lib/Field.js');
+exports.Matrix       = require('./lib/Matrix.js');
+exports.MatrixRing   = require('./lib/MatrixRing.js');
+exports.SquareMatrix = require('./lib/SquareMatrix.js');
+exports.Group        = require('./lib/Group.js');
+exports.Vector       = require('./lib/Vector.js');
+exports.VectorSpace  = require('./lib/VectorSpace.js');
 
 
 var Real = {};
 
-Real.Element            = require('./lib/Real/Element.js');
-Real.Field              = require('./lib/Real/Field.js');
-Real.Vector             = require('./lib/Real/Vector.js');
-Real.VectorSpace        = require('./lib/Real/VectorSpace.js');
-Real.GeneralLinearGroup = require('./lib/Real/GeneralLinearGroup.js');
+Real.Element     = require('./lib/Real/Element.js');
+Real.Field       = require('./lib/Real/Field.js');
+Real.Vector      = require('./lib/Real/Vector.js');
+Real.VectorSpace = require('./lib/Real/VectorSpace.js');
+Real.MatrixRing  = require('./lib/Real/MatrixRing.js');
 
 exports.Real = Real;
 
@@ -22,6 +22,9 @@ var Complex = {};
 
 Complex.Element = require('./lib/Complex/Element.js');
 Complex.Field   = require('./lib/Complex/Field.js');
+//Complex.Vector      = require('./lib/Complex/Vector.js');
+//Complex.VectorSpace = require('./lib/Complex/VectorSpace.js');
+//Complex.MatrixRing  = require('./lib/Complex/MatrixRing.js');
 
 exports.Complex = Complex;
 
@@ -31,4 +34,11 @@ var Quaternion = {};
 //Quaternion.Ring   = require('./lib/Quaternion/Ring.js');
 
 exports.Quaternion = Quaternion;
+
+var Octonion = {};
+
+//Octonion.Element = require('./lib/Octonion/Element.js');
+//Octonion.Ring   = require('./lib/Octonion/Ring.js');
+
+exports.Octonion = Octonion;
 

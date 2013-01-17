6b6b3105afa0995594632268ea74733a61df4c04
fibo <casati_gianluca@yahoo.it>
2013-01-17 23:54:57 +0100

    fixed rightMultiplication

--- test/Real/SquareMatrix.js
+++ test/Real/SquareMatrix.js
@@ -23,27 +23,74 @@ describe('RealSquareMatrix', function () {
   });
 
   describe('rightMultiplication()', function () {
-    it('', function () {
+    it('implements row by column multiplication at right side', function () {
+      var arg = {};
+      arg.order = 2;
+      arg.elements = [2, 0, 0, 2];
+      var elements;
+
+      var matrix1 = new RealSquareMatrix(arg);
+
+      arg.elements = [-1, 0, 0, -1];
+      var matrix2 = new RealSquareMatrix(arg);
+
+      matrix1.rightMultiplication(matrix2);
+
+      elements = matrix1.getElements();
+      assert.ok(elements[0].eq(-2));
+      assert.ok(elements[1].eq(0));
+      assert.ok(elements[2].eq(0));
+      assert.ok(elements[3].eq(-2));
+
+/*
+TODO
+      arg.elements = [-0.5, 0, 0, -0.5];
+      var matrix3 = new RealSquareMatrix(arg);
+
+      elements = matrix3.getElements();
+      assert.ok(elements[0].eq(1));
+      assert.ok(elements[1].eq(0));
+      assert.ok(elements[2].eq(0));
+      assert.ok(elements[3].eq(1));
+*/
     });
   });
 
   describe('leftMultiplication()', function () {
-    it('', function () {
+    it('implements row by column multiplication at left side', function () {
     });
   });
 
   describe('mul()', function () {
-    it('', function () {
+    it('is an alias of rightMultiplication', function () {
+      var arg = {};
+      arg.order = 2;
+
+      var matrix = new RealSquareMatrix(arg);
+
+      assert.ok(matrix.mul === matrix.rightMultiplication);
     });
   });
 
   describe('lmul()', function () {
-    it('', function () {
+    it('is an alias of leftMultiplication', function () {
+      var arg = {};
+      arg.order = 2;
+
+      var matrix = new RealSquareMatrix(arg);
+
+      assert.ok(matrix.lmul === matrix.leftMultiplication);
     });
   });
 
   describe('rmul()', function () {
-    it('', function () {
+    it('is an alias of rightMultiplication', function () {
+      var arg = {};
+      arg.order = 2;
+
+      var matrix = new RealSquareMatrix(arg);
+
+      assert.ok(matrix.rmul === matrix.rightMultiplication);
     });
   });
 });

7c285544dd9af01bb61bafe598287b805a4a0369
fibo <casati_gianluca@yahoo.it>
2013-01-26 14:10:38 +0100

    throws error in determinant is zero

--- test/Real/GeneralLinearGroup.js
+++ test/Real/GeneralLinearGroup.js
@@ -2,13 +2,28 @@
 var assert = require('assert');
 var algebra = require('../../index.js');
 
-var GLnR = algebra.Real.GeneralLinearGroup;
+var GLnR             = algebra.Real.GeneralLinearGroup;
+var RealSquareMatrix = algebra.Real.SquareMatrix;
+
+var GL2 = new GLnR(2);
 
 describe('RealGeneralLinearGroup', function () {
   describe('constructor:', function () {
     it('', function () {
     });
 
+  });
+
+  describe('Matrix', function () {
+    it('is a RealSquareMatrix constructor', function () {
+      var m2 = new GL2.Matrix(1, 2, 3, 4);
+
+      assert.ok(m2 instanceof RealSquareMatrix);
+    });
+
+    it('throws Error', function () {
+      assert.throws(function () { new GL2.Matrix(1, 1, 2, 2); }, Error);
+    });
   });
 });
 

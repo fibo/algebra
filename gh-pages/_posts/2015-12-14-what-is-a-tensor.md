7fcdd2d96aa7efa9e122125961324233e382cbcf
fibo <casati_gianluca@yahoo.it>
2015-12-13 12:10:06 -0500

    added tensor article

---
+++ gh-pages/_posts/2015-12-14-what-is-a-tensor.md
@@ -0,0 +1,9 @@
+---
+title: What is a tensor?
+description: Understanding tensor definition, from plain numbers, through vectors and matrices, to tensors.
+layout: post
+---
+
+There are many definitions for a [Tensor][1]. I will start from scratch by a plain number.
+
+  [1]: https://en.wikipedia.org/wiki/Tensor "Tensor"
--- src/createTensorSpace.js
+++ src/createTensorSpace.js
@@ -1,5 +1,14 @@
-function createTensorSpace () {
+var AbstractTensor = require('./Tensor')
+var AbstractTensorSpace = require('./TensorSpace')
 
+function createTensorSpace (indices, rank) {
+  class TensorSpace extends AbstractTensorSpace {
+    constructor () {
+      super(indices, rank)
+    }
+  }
+
+  return TensorSpace
 }
 
 module.exports = createTensorSpace

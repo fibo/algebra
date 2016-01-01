b7a862a0e6ffd3c2daa7e47ae5c1e88322c368f1
fibo <casati_gianluca@yahoo.it>
2016-01-01 09:02:44 -0500

    addition and subtraction tests

--- test/tensorSpace.js
+++ test/tensorSpace.js
@@ -29,7 +29,10 @@ describe('tensorSpace', () => {
     x.data.should.be.eql(1)
 
     x.addition(2).data.should.be.eql(3)
-    //x.addition(2, 3, 4).data.should.be.eql(10)
+    x.addition(2, 3, 4).data.should.be.eql(10)
+
+    x.subtraction(2).data.should.be.eql(-1)
+    x.subtraction(2, 3, 4).data.should.be.eql(-8)
   })
 
   it('can create a Vector', () => {
@@ -46,6 +49,15 @@ describe('tensorSpace', () => {
 
     Vector.subtraction([2, -1], [1, -1]).should.be.eql([1, 0])
     Vector.subtraction([1, -1], [2, -2], [3, -3]).should.be.eql([-4, 4])
+
+    var v = new Vector([1, 2])
+    v.data.should.be.eql([1, 2])
+
+    v.addition([4, -1]).data.should.be.eql([5, 1])
+    v.addition([4, -1], [-1, 1]).data.should.be.eql([4, 2])
+
+    v.subtraction([2, 1]).data.should.be.eql([-1, 1])
+    v.subtraction([2, 1], [1, -1]).data.should.be.eql([-2, 2])
   })
 
   it('can create a Matrix', () => {
@@ -59,31 +71,39 @@ describe('tensorSpace', () => {
     Matrix.order.should.be.eql(2)
 
     Matrix.addition([1, 0,
-                     0, 1],
-                    [1, -1,
-                     0,  1]).should.be.eql([2, -1,
-                                            0,  2])
+                     0, 1], [1, -1,
+                             0,  1]).should.be.eql([2, -1,
+                                                    0,  2])
 
     Matrix.addition([1, 0,
-                     0, 1],
-                    [1, -1,
-                     0,  1],
-                    [2, 1,
-                     1, 2]).should.be.eql([4, 0,
-                                           1, 4])
+                     0, 1], [1, -1,
+                             0,  1], [2, 1,
+                                      1, 2]).should.be.eql([4, 0,
+                                                            1, 4])
 
     Matrix.subtraction([1, 0,
-                        0, 1],
-                       [1, -1,
-                        0,  1]).should.be.eql([0, 1,
-                                               0, 0])
+                        0, 1], [1, -1,
+                                0,  1]).should.be.eql([0, 1,
+                                                       0, 0])
 
     Matrix.subtraction([1, 0,
-                        0, 1],
-                       [1, -1,
-                        0,  1],
-                       [2, 1,
-                        1, 2]).should.be.eql([-2,  0,
-                                              -1, -2])
+                        0, 1], [1, -1,
+                                0,  1], [2, 1,
+                                         1, 2]).should.be.eql([-2,  0,
+                                                               -1, -2])
+
+    var m = new Matrix([1, 2,
+                        3, 4])
+
+    m.data.should.be.eql([1, 2,
+                          3, 4])
+
+    m.addition([1, 0,
+                0, 1]).data.should.be.eql([2, 2,
+                                           3, 5])
+
+    m.subtraction([1, 0,
+                   0, 1]).data.should.be.eql([0, 2,
+                                              3, 3])
   })
 })

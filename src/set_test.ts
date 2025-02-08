ba8d55ffef56861489d7c1af93e4d5356be6264c
Gianluca Casati <fibo@users.noreply.github.com>
2025-02-08 12:36:26 +0700

    remove ts-node

--- src/set_test.ts
+++ src/set_test.ts
@@ -1,6 +1,6 @@
 import { strict as assert } from "node:assert";
 import { describe, it } from "node:test";
-import { AlgebraSet } from "./set.js";
+import type { AlgebraSet } from "./set.ts";
 
 describe("AlgebraSet", () => {
 	it("is used to create a set of elements", () => {
